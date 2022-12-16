56895ad8757c5bd4c672849cad85a5795c7f8070
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-12-16 00:29:15 -0500

    Created controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,12 @@
 class PicturesController < ApplicationController
+
+  skip_before_action :authenticate_user!
+
+  def index
+    @pictures = Picture.all
+  end
+  
+  def show
+    @picture = Picture.find(params[:id])
+  end
 end
