23675f4ee0455d27921880ba190686dbfc8b7f88
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-12-16 00:20:56 -0500

    Added Seed and Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Picture.destroy_all
+
+puts "Creating pictures..."
+10.times do
+  Picture.create(
+    name: Faker::Games::Pokemon.name,
+    description: Faker::Games::Pokemon.move,
+    category: Faker::Book.title ,
+    price: rand(15..150),
+    is_home_item: false,
+    is_portfolio_item: true,
+    is_sketchbook_item: false,
+    is_shopping_item: false,
+  )
+end
